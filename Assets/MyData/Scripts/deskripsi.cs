e7eeb397b76c511dd6992b7977af83d5263584c0
Faris-Ihsan <farisihsan32@yahoo.co.id>
2021-01-27 10:04:35 +0700

    jadi

---
+++ Assets/MyData/Scripts/deskripsi.cs
@@ -0,0 +1,113 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Proyecto26;
+
+public class deskripsi : MonoBehaviour
+{
+    public Text textnamaruangan, textposisi, textdeskripsiruangan;
+    public float x, y, z;
+    public bool canvas = false;
+    public string check, posisiruangan;
+
+    public GameObject[] objects;    
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //set text faris
+        //textnamaruangan.text = x.ToString("F0")+y.ToString("F0")+z.ToString("F0");
+        //textnamaruangan.text = "Faris");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getposisi();
+
+        if (check == posisiruangan)
+        {
+            AAAAAAAAAAAAAAAAAA();
+        }else
+        {
+            hilangkanCanvas();
+        }
+    }
+
+    // Method untuk mencari posisi player(camera/user)
+    private void getposisi()        
+    {
+        //variable mencari posisi GameObject yang berupa player 
+        var pos = GameObject.Find("Player").transform.position;
+        var cetak = x.ToString("F0")+ "," +y.ToString("F0")+ "," +z.ToString("F0");
+        check = x.ToString("F0")+y.ToString("F0")+z.ToString("F0");
+        
+        //memasukan posisi ke dalam variable x,y,z yang berupa float
+        x = pos.x;
+        y = pos.y;
+        z = pos.z;
+        Vector3 point = new Vector3(x,y,z);
+        
+        //print posisi ke text UI
+        textposisi.text = "Posisi(x,y,z): "+ cetak;
+
+
+        //Ambil Data Nama Ruangan
+        Debug.Log(check);
+       
+        RestClient.Get("https://vr-proyek3-default-rtdb.firebaseio.com/ga/" + check + "/posisi.json").Then(response => 
+        { 
+            posisiruangan = response.Text;
+        });
+
+         
+    }
+
+    void AAAAAAAAAAAAAAAAAA()
+    {
+         RestClient.Get("https://vr-proyek3-default-rtdb.firebaseio.com/ga/" + check + "/nama_ruangan.json").Then(response => 
+        { 
+            if (response.Text == "null")
+            {
+                hilangkanCanvas();
+                textnamaruangan.text = "Ruangan: ";
+            }else
+            {
+                munculkanCanvas();
+                textnamaruangan.text = "Ruangan: " + response.Text;
+            }
+        });
+
+        //Ambil data Deskripsi Ruangan
+        RestClient.Get("https://vr-proyek3-default-rtdb.firebaseio.com/ga/" + check + "/deskripsi_ruangan.json").Then(response => 
+        { 
+            if (response.Text == "null")
+            {
+                textdeskripsiruangan.text = " ";
+            }else
+            {
+                textdeskripsiruangan.text = response.Text;
+            }
+        });
+    }
+
+
+    void hilangkanCanvas()
+    {
+        foreach (GameObject go in objects)
+        {
+            go.SetActive(false);
+        }
+    }
+
+    void munculkanCanvas()
+    {
+        foreach (GameObject go in objects)
+        {
+            go.SetActive(true);
+        }
+    }
+    
+}
---
+++ Assets/MyData/Scripts/playerMaju.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMaju : MonoBehaviour
+{
+	
+    public int kecepatanPlayer;
+    public float x, y, z;
+    public double xcek, ycek, zcek;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Maju();
+        GetPosisi();
+        cek();
+    }
+
+    void Maju()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+        	transform.position = transform.position + Camera.main.transform.forward * kecepatanPlayer * Time.deltaTime;
+        }
+    }
+
+    void GetPosisi()
+    {
+        x = transform.position.x;
+        y = transform.position.y;
+        z = transform.position.z;
+
+        Vector3 point = new Vector3(x,y,z);
+
+        //Debug.Log(point);
+    }
+
+    void cek()
+    { 
+        /*
+        xcek =  0.0;
+        ycek =  1.4;
+        zcek = -8.0;
+
+        if (x >= xcek && y >= ycek && z >= zcek)
+        {
+            Debug.Log("POSISI TEPAT");
+        }
+        */
+    }
+}
